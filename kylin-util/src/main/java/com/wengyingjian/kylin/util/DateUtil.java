30c0292fc360ac67e4ed438269c754d056713dd8
wengyingjian <q291611265@gmail.com>
2016-01-22 22:51:29 +0800

    添加日期转

--- kylin-util/src/main/java/com/wengyingjian/kylin/util/DateUtil.java
+++ kylin-util/src/main/java/com/wengyingjian/kylin/util/DateUtil.java
@@ -3,10 +3,12 @@
  */
 package com.wengyingjian.kylin.util;
 
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
- *
  * @author <a href="mailto:chenyb@59store.com">山人</a>
  * @version 1.0 15/11/6
  * @since 1.0
@@ -16,8 +18,7 @@ public class DateUtil {
     /**
      * 根据Unix时间戳计算那一天开始的时间戳
      *
-     * @param time
-     *            给定任意一点时间, 精确到毫秒
+     * @param time 给定任意一点时间, 精确到毫秒
      * @return 那一天开始的时间, 精确到秒
      */
     public static int getStartTimeOfOneDay(long time) {
@@ -27,6 +28,28 @@ public class DateUtil {
         cal.set(Calendar.MINUTE, 0);
         cal.set(Calendar.SECOND, 0);
         return (int) (cal.getTimeInMillis() / 1000);
+    }
+
+    /**
+     * 日期转字符串
+     *
+     * @param date    日期
+     * @param pattern 格式
+     */
+    public static String format(Date date, String pattern) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+        return simpleDateFormat.format(date);
+    }
+
+    /**
+     * 字符串转日期
+     *
+     * @param date    字符串
+     * @param pattern 格式
+     */
+    public static Date format(String date, String pattern) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        return simpleDateFormat.parse(date, new ParsePosition(0));
     }
 
 }
--- kylin-util/src/main/java/com/wengyingjian/kylin/util/StringUtil.java
+++ kylin-util/src/main/java/com/wengyingjian/kylin/util/StringUtil.java
@@ -1,7 +1,10 @@
 package com.wengyingjian.kylin.util;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * Created by wengyingjian on 16/1/22.
  */
 public class StringUtil {
+
 }
