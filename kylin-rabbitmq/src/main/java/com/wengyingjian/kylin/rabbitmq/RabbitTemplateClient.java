7ea65b18923e04d33886dc4398dcf610b66f5667
wengyingjian <q291611265@gmail.com>
2016-01-23 16:02:04 +0800

    解决mq的一些问题

--- kylin-rabbitmq/src/main/java/com/wengyingjian/kylin/rabbitmq/RabbitTemplateClient.java
+++ kylin-rabbitmq/src/main/java/com/wengyingjian/kylin/rabbitmq/RabbitTemplateClient.java
@@ -1,9 +1,20 @@
 package com.wengyingjian.kylin.rabbitmq;
 
+import com.wengyingjian.kylin.util.JsonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by wengyingjian on 16/1/23.
  */
-@Componment
+@Component
 public class RabbitTemplateClient {
     private Logger logger = LoggerFactory.getLogger(RabbitTemplateClient.class);
 
@@ -13,11 +24,11 @@ public class RabbitTemplateClient {
     @Value("${rabbitmq.event.entry}")
     private String queueName;
 
-    public void sendMessgeToMq(MessageBean messageBean){
-        Map<String,Object> map = new HashMap<String,Object>();
+    public void sendMessgeToMq(MessageBean messageBean) {
+        Map<String, Object> map = new HashMap<String, Object>();
         map.put("target", "PUSH");
         map.put("param", messageBean);
-        String str = JsonUtils.toJson(map);
+        String str = JsonUtil.getJsonFromObject(map);
         logger.info("send message to MQ:" + str);
         rabbitTemplate.convertAndSend(queueName, str);
     }
