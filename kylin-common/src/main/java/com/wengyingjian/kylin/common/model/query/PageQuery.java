bd068264718918851d39e5e37de5d1dd7ac7b2a4
wengyingjian <q291611265@gmail.com>
2016-02-01 22:17:57 +0800

    kylin.common.PageQuery:添加是否启动分页标记

--- kylin-common/src/main/java/com/wengyingjian/kylin/common/model/query/PageQuery.java
+++ kylin-common/src/main/java/com/wengyingjian/kylin/common/model/query/PageQuery.java
@@ -4,21 +4,26 @@ import java.io.Serializable;
 
 /**
  * 分页查询父类,查询类可继承此类<br/>
+ * 是否启用分页查询取决于 pageIndex == null
  * <p>
- * 默认参数: 页码 = 1 ,大小 = 100
  * Created by wengyingjian on 16/2/1.
  */
 public class PageQuery implements Serializable {
 
-    private static final int DEFAULT_INDEX = 1;
     private static final int DEFAULT_SIZE = 100;
-
-    private int pageIndex;
+    //是否启用分页查询
+    private Boolean pageQuery;
+    private Integer pageIndex;
     //default size
     private int pageSize;
 
 
-    public void setPageIndex(int pageIndex) {
+    public void setPageIndex(Integer pageIndex) {
+        if (pageIndex == null) {
+            this.pageQuery = null;
+            return;
+        }
+        this.pageQuery = true;
         this.pageIndex = pageIndex;
     }
 
@@ -30,7 +35,13 @@ public class PageQuery implements Serializable {
      * @return offset if pageIndex was set , default 0
      */
     public Integer getOffset() {
-        return pageIndex * pageSize > 0 ? (pageIndex - 1) * pageSize : DEFAULT_INDEX;
+        if (pageIndex == null) {
+            return null;
+        }
+        if (pageIndex <= 0) {
+            return 1;
+        }
+        return (pageIndex - 1) * pageSize;
     }
 
     /**
@@ -39,4 +50,5 @@ public class PageQuery implements Serializable {
     public Integer getLimit() {
         return pageSize > 0 ? pageSize : DEFAULT_SIZE;
     }
+
 }
