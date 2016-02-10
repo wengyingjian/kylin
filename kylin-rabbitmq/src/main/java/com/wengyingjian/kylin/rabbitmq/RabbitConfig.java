3298b289591ef342aee070640052cf7527a0e710
wengyingjian <q291611265@gmail.com>
2016-02-10 09:59:49 +0800

    kylin.rabbitmq:add rabbitmq config

---
+++ kylin-rabbitmq/src/main/java/com/wengyingjian/kylin/rabbitmq/RabbitConfig.java
@@ -0,0 +1,11 @@
+package com.wengyingjian.kylin.rabbitmq;
+
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * Created by wengyingjian on 16/2/10.
+ */
+@PropertySource("classpath:rabbitmq.properties")
+public class RabbitConfig {
+
+}
